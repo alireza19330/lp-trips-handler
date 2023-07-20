91c8c75febbebfb2ccfebc243e0014971b9b008f
Alireza Abazari <alireza.abazari@sportsbet.com.au>
2023-07-21 02:04:53 +1000

    update TapDetailsFormatException

--- src/main/java/com/lp/triphandler/application/exception/TapDetailsFormatException.java
+++ src/main/java/com/lp/triphandler/application/exception/TapDetailsFormatException.java
@@ -1,4 +1,7 @@
 package com.lp.triphandler.application.exception;
 
 public class TapDetailsFormatException extends RuntimeException {
+    public TapDetailsFormatException(String message) {
+        super(message);
+    }
 }
